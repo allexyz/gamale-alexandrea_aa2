a9202c050ea12bd892f01e2eaac7806ecbe83f45
Alex <125948626+allexyz@users.noreply.github.com>
2023-03-28 18:49:30 +0800

    Add files via upload

---
+++ do-while.js
@@ -0,0 +1,9 @@
+var y = 2;
+
+do{
+	document.write(y);
+	if(y < 50){
+		document.write(", ");
+	}
+	y+=2;
+}while(y < 51);
---
+++ while.js
@@ -0,0 +1,9 @@
+var x = 1;
+
+while(x < 52){
+	document.write(x);
+	if(x < 51){
+		document.write(", ");
+	}
+	x+=2;
+}
